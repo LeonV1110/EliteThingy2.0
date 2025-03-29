25fb09706f85c8a06abd4ebea0ffff4c19a145f6
LeonV1110 <59734771+LeonV1110@users.noreply.github.com>
2025-03-29 20:25:25 +0100

    Flask implementation + data update + enums update + volcanism logic update

--- app/enums.py
+++ app/enums.py
@@ -13,67 +13,83 @@ def list_json_to_enum(ls:list[str], cls):
     return res
 
 class Starclass(Enum):
-    O = 0
-    B = 1
-    A = 2
-    F = 3
-    G = 4
-    K = 5
-    M = 6
-    Be = 7
-    TTS = 8
-    C = 9
-    S = 10
-    W = 11
-    Black_Hole = 12
-    Neutron = 13
-    D = 14
-    L = 15
-    T = 16
-    Y = 17
-    Any = 18
-    Ae = 19
+    O           = 0
+    B           = 1
+    A           = 2
+    F           = 3
+    G           = 4
+    K           = 5
+    M           = 6
+    Be          = 7
+    TTS         = 8
+    C           = 9
+    S           = 10
+    W           = 11
+    Black_Hole  = 12
+    Neutron     = 13
+    D           = 14
+    L           = 15
+    T           = 16
+    Y           = 17
+    Any         = 18
+    Ae          = 19
+    N           = 20
     
 
 class PlanetType(Enum):
-    Rock = 0
-    HMC = 1
-    Ice = 2
-    RockIce = 3
+    Rock        = 0
+    HMC         = 1
+    Ice         = 2
+    RockIce     = 3
 
 class AtmossphericComposition(Enum):
-    NH3 = 0
-    CO2 = 1
-    Ar = 2
-    CH4 = 3
-    O = 4
-    Ni = 5
-    Ne = 6
-    SO2 = 7
-    H2O = 8
-    He = 9
-    Any = 10
+    NH3     = 0
+    CO2     = 1
+    Ar      = 2
+    CH4     = 3
+    O       = 4
+    Ni      = 5
+    Ne      = 6
+    SO2     = 7
+    H2O     = 8
+    He      = 9
+    Any     = 10
 
 
 class VolcanismType(Enum):
-    none = 0
-    Any = 1
-    Low = 2
+    none                    = 0
+    Any                     = 1
+    Low                     = 2
+    WaterMagma              = 3
+    SulphurDioxideMagma     = 4
+    AmmoniaMagma            = 5
+    MethaneMagma            = 6
+    NitrogenMagma           = 7
+    SilicateMagma           = 8
+    MetalicMagma            = 9
+    WaterGeyser             = 10
+    CarbonGeyser            = 11
+    AmmoniaGeyser           = 12
+    MethanGeyser            = 13
+    NitrogenGeyser          = 14
+    HeliumGeyser            = 15
+    SilicateGeyser          = 16
+    RockyMagma              = 17
+
 
 class BioType(Enum):
-    Bacterium =  0
-    Fonticulua = 1
-    Aleoida = 2
-    Tussock = 3
-    Cactoida = 4
-    Clypeus = 5
-    Concha = 6
-    Electicae = 7
-    Frutexa = 8
-    Fumerola = 9
-    Fungoida = 10
-    Osseus = 11
-    Recepta = 12
-    Tubus = 13
-    Stratum = 14
-
+    Bacterium   =  0
+    Fonticulua  = 1
+    Aleoida     = 2
+    Tussock     = 3
+    Cactoida    = 4
+    Clypeus     = 5
+    Concha      = 6
+    Electicae   = 7
+    Frutexa     = 8
+    Fumerola    = 9
+    Fungoida    = 10
+    Osseus      = 11
+    Recepta     = 12
+    Tubus       = 13
+    Stratum     = 14
---
+++ app/flaskApp.py
@@ -0,0 +1,43 @@
+import flask
+from body import Body
+from enums import Starclass, PlanetType, AtmossphericComposition, VolcanismType, BioType
+from input import Input
+from main import get_possible_biologicals, get_most_valuable_biologicals, get_least_valuable_biologicals
+from pathlib import Path
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    
+    output = "Path.cwd()"
+    if flask.request.method == 'POST':
+
+        bioCount = int(flask.request.form.get('bioCount'))
+        starclass = Starclass[(flask.request.form.get('starclass'))]
+        planetType = PlanetType[(flask.request.form.get('planetType'))]
+        atmossphericComposition = AtmossphericComposition[(flask.request.form.get('atmossphericComposition'))]
+        volcanismType = VolcanismType[(flask.request.form.get('volcanismType'))]
+        atmossphericPressure = float(flask.request.form.get('atmossphericPressure'))
+        gravity = float(flask.request.form.get('gravity'))
+        minTemp = int(flask.request.form.get('minTemp'))
+        maxTemp = int(flask.request.form.get('maxTemp'))
+
+        
+        body = Body('', 0, '', 0, bioCount, starclass, planetType, atmossphericComposition, volcanismType, atmossphericPressure, gravity, minTemp, maxTemp)
+
+        all_bio = Input.read_biologicals("C:\\Users\\Leonl\\Documents\\code projects\\EliteThingy2.0\\app\\biologicals")
+
+
+        jsonBody = body.to_json()
+        leastValue = get_least_valuable_biologicals(body, all_bio)
+        mostValue = get_most_valuable_biologicals(body, all_bio)
+        allPossible = get_possible_biologicals(body, all_bio)
+
+        sortedAllPossible = sorted(allPossible, key= lambda x: x.value)
+        sortedAllPossible.reverse()
+    return flask.render_template('index.html', jsonBody = jsonBody, leastValue = leastValue, mostValue = mostValue, allPossible = sortedAllPossible, Path = Path)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
