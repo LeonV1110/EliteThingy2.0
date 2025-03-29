500327d6b14303e4800295942ef14e7d47286af9
LeonV1110 <59734771+LeonV1110@users.noreply.github.com>
2025-03-29 22:49:04 +0100

    basic graceful error handling

--- app/flaskApp.py
+++ app/flaskApp.py
@@ -3,53 +3,55 @@ from body import Body
 from enums import Starclass, PlanetType, AtmossphericComposition, VolcanismType, BioType
 from input import Input
 from main import get_possible_biologicals, get_most_valuable_biologicals, get_least_valuable_biologicals, get_total_value
-from pathlib import Path
 
 app = flask.Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     
-    output = "Path.cwd()"
     jsonBody = []
     leastValue = []
     mostValue = []
     sortedAllPossible = []
     totalLeast = -1
     totalMost = -1
+    error = ''
     if flask.request.method == 'POST':
+        try:
+            bioCount = int(flask.request.form.get('bioCount'))
+            starclass = Starclass[(flask.request.form.get('starclass'))]
+            planetType = PlanetType[(flask.request.form.get('planetType'))]
+            atmossphericComposition = AtmossphericComposition[(flask.request.form.get('atmossphericComposition'))]
+            volcanismType = VolcanismType[(flask.request.form.get('volcanismType'))]
+            atmossphericPressure = float(flask.request.form.get('atmossphericPressure'))
+            gravity = float(flask.request.form.get('gravity'))
+            minTemp = int(flask.request.form.get('minTemp'))
+            maxTemp = int(flask.request.form.get('maxTemp'))
 
-        bioCount = int(flask.request.form.get('bioCount'))
-        starclass = Starclass[(flask.request.form.get('starclass'))]
-        planetType = PlanetType[(flask.request.form.get('planetType'))]
-        atmossphericComposition = AtmossphericComposition[(flask.request.form.get('atmossphericComposition'))]
-        volcanismType = VolcanismType[(flask.request.form.get('volcanismType'))]
-        atmossphericPressure = float(flask.request.form.get('atmossphericPressure'))
-        gravity = float(flask.request.form.get('gravity'))
-        minTemp = int(flask.request.form.get('minTemp'))
-        maxTemp = int(flask.request.form.get('maxTemp'))
+            
+            body = Body('', 0, '', 0, bioCount, starclass, planetType, atmossphericComposition, volcanismType, atmossphericPressure, gravity, minTemp, maxTemp)
 
-        
-        body = Body('', 0, '', 0, bioCount, starclass, planetType, atmossphericComposition, volcanismType, atmossphericPressure, gravity, minTemp, maxTemp)
-
-        all_bio = Input.read_biologicals("app\\biologicals")
+            all_bio = Input.read_biologicals("app\\biologicals")
 
 
-        jsonBody = body.to_json()
-        leastValue = get_least_valuable_biologicals(body, all_bio)
-        mostValue = get_most_valuable_biologicals(body, all_bio)
-        allPossible = get_possible_biologicals(body, all_bio)
+            jsonBody = body.to_json()
+            leastValue = get_least_valuable_biologicals(body, all_bio)
+            mostValue = get_most_valuable_biologicals(body, all_bio)
+            allPossible = get_possible_biologicals(body, all_bio)
 
-        totalLeast = f"{get_total_value(body, leastValue):,}".replace(",",".")
-        totalMost = f"{get_total_value(body, mostValue):,}".replace(",",".")
+            totalLeast = f"{get_total_value(body, leastValue):,}".replace(",",".")
+            totalMost = f"{get_total_value(body, mostValue):,}".replace(",",".")
 
-        sortedAllPossible = sorted(allPossible, key= lambda x: x.value)
-        sortedAllPossible.reverse()
+            sortedAllPossible = sorted(allPossible, key= lambda x: x.value)
+            sortedAllPossible.reverse()
+        except ValueError as e:
+            error = 'You entered a wrong value, please try again'
     return flask.render_template('index.html', 
-                                 totalLeast = totalLeast, totalMost=totalMost, 
-                                 jsonBody = jsonBody, 
-                                 leastValue = leastValue, mostValue = mostValue, 
-                                 allPossible = sortedAllPossible, Path = Path)
+                                    error = error,
+                                    totalLeast = totalLeast, totalMost=totalMost, 
+                                    jsonBody = jsonBody, 
+                                    leastValue = leastValue, mostValue = mostValue, 
+                                    allPossible = sortedAllPossible)
 
 
 if __name__ == '__main__':
