a402c2ee1e75c49508bc149988ab30829277eba7
LeonV1110 <59734771+LeonV1110@users.noreply.github.com>
2026-08-10 11:51:00 +0200

    Last windows push

--- reading_spansh_testing/Input.py
+++ reading_spansh_testing/Input.py
@@ -1,13 +1,14 @@
 import ijson
 import os
 
-def get_checkpoint(checkpoint_path: str)-> int:
+#returns bytepos (compressed), json object
+def get_checkpoint(checkpoint_path: str)-> (int, int):
     if os.path.exists(checkpoint_path):
         with open(checkpoint_path, 'r') as f:
             return #TODO
         
     else:
-        return 0
+        return (0,0)
 
 
 def read_file(filepath: str, checkpoint_path: str, indexes:int = 10000):
